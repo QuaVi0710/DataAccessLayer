3f3d7cd530e31781cf1407e0326ddc2c51fea601
VinhTQ <vinhtqde170661@fpt.edu.vn>
2025-05-26 15:21:42 +0700

    commit dto and model

---
+++ BussinessObjects/DTOs/Request/Company/CompanyForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.DTOs.Request.Company
+{
+    public class CompanyForCreationDto : CompanyForManipulationDto
+    {
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
+    }
+}
---
+++ BussinessObjects/DTOs/Request/EmployeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.DTOs.Request
+{
+    public class EmployeeDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+
+        // Có thể thêm các trường computed nếu cần
+        public string FullInfo => $"{Name}, {Age} years old, {Position}";
+    }
+}
